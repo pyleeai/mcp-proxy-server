d66916ebd49e67ba157ab4bd7822671c85bdfad0
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-04-21 15:36:01 -0400

    feat(request): add listRequestHandler to aggregate client data asynchronously

---
+++ src/request.ts
@@ -0,0 +1,51 @@
+import type { z } from "zod";
+import { getAllClients } from "./data";
+import { logger } from "./logger";
+import type {
+	ListRequestHandlerCallback,
+	ListRequestHandlerConfig,
+} from "./types";
+
+using log = logger;
+
+export function listRequestHandler<
+	P extends Record<string, unknown>,
+	R extends object,
+>(
+	schema: z.ZodType<R>,
+	config: ListRequestHandlerConfig<R>,
+	callback: ListRequestHandlerCallback,
+) {
+	return async ({ params }: { method: string; params?: P }) => {
+		const { method, key } = config;
+		const clients = getAllClients();
+		const response = {
+			[key as string]: (
+				await Promise.all(
+					clients.map(async (client) => {
+						try {
+							log.debug(`Collecting ${method} from ${client.name}`);
+							const result = await client.client.request(
+								{ method, params },
+								schema,
+							);
+							log.debug(`Collected ${method} from ${client.name}`);
+							const items = result[key];
+							return Array.isArray(items)
+								? items.map((item) => callback(client, item))
+								: [];
+						} catch (error) {
+							log.error(
+								`Error collecting ${method} from ${client.name}`,
+								error,
+							);
+							return [];
+						}
+					}),
+				)
+			).flat(),
+		} as R;
+
+		return response;
+	};
+}
