bc152e1811bc9afbd1b30242f530aaf03de621b1
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-04-22 20:12:39 -0400

    feat(errors): add ClientRequestError class for request errors

--- src/errors.ts
+++ src/errors.ts
@@ -7,3 +7,13 @@ export class ConfigurationError extends Error {
 		this.name = "ConfigurationError";
 	}
 }
+
+export class ClientRequestError extends Error {
+	constructor(
+		message: string,
+		public cause?: unknown,
+	) {
+		super(message, { cause });
+		this.name = "ClientRequestError";
+	}
+}
