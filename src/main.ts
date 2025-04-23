185a55bdf452805d5b44f6a04f4096d566abb670
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-04-23 18:59:22 -0400

    feat: add main entry point with server and signal handling fix(main): update server initialization and error handling to simplify startup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+import { exitWithError, handleSIGINT } from "./process";
+import { proxy } from "./proxy";
+
+export async function main() {
+	await proxy();
+}
+
+main().catch(exitWithError);
+
+process.on("SIGINT", handleSIGINT);
